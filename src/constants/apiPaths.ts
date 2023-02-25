020e52de41a6baf98719fd2b89c360a0dd0d917a
Volha_Manko <volha.manko@workhuman.com>
2023-02-25 17:14:10 +0100

    [Task-3] Updated paths for product list and product

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,5 @@
 const API_PATHS = {
-  product:
-    "https://x1m10ux2x7.execute-api.eu-central-1.amazonaws.com/dev/products",
+  product: "https://x1m10ux2x7.execute-api.eu-central-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
