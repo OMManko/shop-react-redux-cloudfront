0fd986a818930c030dafb84bb5ba851d85e4d247
Volha_Manko <volha.manko@workhuman.com>
2023-03-25 15:31:07 +0100

    [Task-7] Added authentication logic

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://3zi2k6ab2e.execute-api.eu-central-1.amazonaws.com/dev",
   order: "https://3zi2k6ab2e.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://uz90pjf54i.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://ogf8m7mmcf.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://3zi2k6ab2e.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://3zi2k6ab2e.execute-api.eu-central-1.amazonaws.com/dev",
 };
